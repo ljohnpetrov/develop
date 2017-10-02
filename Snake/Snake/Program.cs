60466fd3f51d94c9fa2c83e968dd2f2c3cb1083e
glhf <hant15@yandex.ru>
2017-10-02 14:39:14 +0300

    ola

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -4,24 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Snake
-{
-    class Program
+
+    namespace Snake
     {
-        static void Main(string[] args)
+        class Program
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-            Console.SetCursorPosition(x1, y1);
-            Console.Write(sym1);
-            
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-            Console.SetCursorPosition(x2, y2);
-            Console.Write(sym2);
-            Console.ReadLine();
+            static void Main(string[] args)
+            {
+                int x1 = 1;
+                int y1 = 3;
+                char sym1 = '*';
+                Draw(x1, y1, sym1);
+
+
+                int x2 = 4;
+                int y2 = 5;
+                char sym2 = '#';
+                Draw(x2, y2, sym2);
+            }
+            static void Draw(int x, int y, char sym)
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write(sym);
+            }
         }
     }
-}
